8dda0132d32a46177413ee92a7305fde0b82cc16
JuanPrato <jprato@fi.uba.ar>
2021-10-06 19:41:19 -0300

    clean first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: "**",
-    loadChildren: () => import('./login/login.module').then((m) => m.LoginModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBKxyg35BRYB8gRJd1mW4FBe3ZLzZlVAR0",
+    authDomain: "ms-diagramer.firebaseapp.com",
+    projectId: "ms-diagramer",
+    storageBucket: "ms-diagramer.appspot.com",
+    messagingSenderId: "671367335383",
+    appId: "1:671367335383:web:c5e81b78a76ec915718f92",
+    measurementId: "G-3V26ETW9CW"
+  }
 };
